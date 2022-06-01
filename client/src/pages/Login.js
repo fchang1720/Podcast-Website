cbbe0bb4cc992438610c74f4c8546d29523badcd
Cumar Cumar <umar-19@live.com>
2022-06-01 14:19:04 -0500

    added styling for login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -4,6 +4,7 @@ import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 
 import Auth from '../utils/auth';
+import Form from 'react-bootstrap/Form';
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
@@ -51,33 +52,56 @@ const Login = (props) => {
                 Success! You may now head{' '}
                 <Link to="/">back to the homepage.</Link>
               </p>
-            ) : (
-              <form onSubmit={handleFormSubmit}>
-                <input
-                  className="form-input"
-                  placeholder="Your email"
-                  name="email"
-                  type="email"
-                  value={formState.email}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="******"
-                  name="password"
-                  type="password"
-                  value={formState.password}
-                  onChange={handleChange}
-                />
-                <button
-                  className="btn btn-block btn-primary"
-                  style={{ cursor: 'pointer' }}
-                  type="submit"
-                >
-                  Submit
-                </button>
-              </form>
-            )}
+            ) : 
+          // form inputs for the login page
+          <Form onSubmit={handleFormSubmit}>  
+          <Form.Group className="mb-3 form-input" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" name='email' value={formState.email} onChange={handleChange}/>
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+        <Form.Group className="mb-3 form-input" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="******" name='password' value={formState.password} onChange={handleChange}/>
+        </Form.Group>
+        
+        <button variant="primary" type="submit" className="btn btn-block btn-primary">
+            Submit
+        </button>
+        </Form>
+            // (
+            //   <form onSubmit={handleFormSubmit}>
+            //     <input
+            //       className=" mb -3 form-input"
+            //       placeholder="Your email"
+            //       name="email"
+            //       type="email"
+            //       value={formState.email}
+            //       onChange={handleChange}
+            //     />
+            //     <input
+            //       className="form-input"
+            //       placeholder="******"
+            //       name="password"
+            //       type="password"
+            //       value={formState.password}
+            //       onChange={handleChange}
+            //     />
+            //     <button
+            //       className="btn btn-block btn-primary"
+            //       style={{ cursor: 'pointer' }}
+            //       type="submit"
+            //     >
+            //       Submit
+            //     </button>
+            //   </form>
+            // )
+           
+            }
+       
 
             {error && (
               <div className="my-3 p-3 bg-danger text-white">
