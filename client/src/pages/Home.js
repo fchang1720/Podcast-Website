6cb06ebeb616484eb52d717c57d941a2566a7bcf
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-06-01 10:38:10 -0500

    worked on minor html changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -12,25 +12,35 @@ const Home = () => {
 
   return (
     <main>
+
+      <div className='homepage-container'>
+
+        <h1 className='homepage-title'>Welcome!</h1>
+        <hr />
+        <p className='homepage-message'>This site is a place to share your own podcasts.
+        Feel free to create your own posts, as well as comment on others!
+        Be as creative as you can be, and have fun!
+        </p>
+
+      </div>
+
       <div className="flex-row justify-center">
-        <div
-          className="col-12 col-md-10 mb-3 p-3"
-          style={{ border: '1px dotted #1a1a1a' }}
-        >
-          <a href='/me'>Post Form</a>
+
+        {/* <div className="col-12 col-md-10 mb-3 p-3" style={{ border: '1px dotted #1a1a1a' }}>
+          <a href='/me'>Create a post!</a>
           {/* <PostForm /> */}
-        </div>
+        {/* </div> */}
+
         <div className="col-12 col-md-8 mb-3">
           {loading ? (
             <div>Loading...</div>
           ) : (
-            <PostList
-              posts={posts}
-              title="See what Others are posting"
-            />
+            <PostList posts={posts} title="See what others are posting below!"/>
           )}
         </div>
+
       </div>
+
     </main>
   );
 };
