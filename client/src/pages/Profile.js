5facaa1fa2f88eeabe7733be49dabf1517f9fa2e
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-06-01 14:22:34 -0500

    adjusted some spacing issues

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -36,7 +36,7 @@ const Profile = () => {
   }
 
   return (
-    <div>
+    <div className='profile-posts'>
       <div className="flex-row justify-center mb-3">
         <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
           Viewing {userParam ? `${user.username}'s` : 'your'} profile.
